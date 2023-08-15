9652dfadf2a8eef3475f99ea4dad37208d8d083e
hermittt <499655727@qq.com>
2023-08-15 11:09:21 +0000

    fashion的模型lr不行，换成和mnist一样

--- flgo/benchmark/fashion_classification/__init__.py
+++ flgo/benchmark/fashion_classification/__init__.py
@@ -1,8 +1,8 @@
 import flgo.benchmark.toolkits.visualization
-from flgo.benchmark.fashion_classification.model import lr
+from flgo.benchmark.fashion_classification.model import cnn
 import flgo.benchmark.toolkits.partition
 
 default_partitioner = flgo.benchmark.toolkits.partition.IIDPartitioner
 default_partition_para = {'num_clients':100}
-default_model = lr
+default_model = cnn #换成和mnist一样
 visualize = flgo.benchmark.toolkits.visualization.visualize_by_class
