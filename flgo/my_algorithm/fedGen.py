dab1ef2840fd7cca156265714f714692f6fc355d
hermittt <499655727@qq.com>
2024-06-16 16:45:46 +0000

    task

--- flgo/my_algorithm/fedGen.py
+++ flgo/my_algorithm/fedGen.py
@@ -1,47 +1,67 @@
 from flgo.utils import fmodule
 import flgo
+import numpy as np
 import torch,copy
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from flgo.algorithm.fedbase import BasicServer, BasicClient
+from flgo.my_algorithm.extra import extraServer, extraClient
 from dataclasses import dataclass
 import flgo.algorithm.fedavg as fedavg
 
-from my_utils import grad_False,grad_True,get_loc_data
-
+from flgo.my_algorithm.my_utils import grad_False,grad_True,get_loc_data
 
 # based on official code https://github.com/zhuangdizhu/FedGen/blob/main/FLAlgorithms/trainmodel/generator.py
+
 class Generative_fedGEN(nn.Module):
-    def __init__(self, noise_dim, num_classes, hidden_dim, feature_dim, device) -> None:
-        super().__init__()
+  def __init__(self, noise_dim, num_classes, hidden_dim, feature_dim, device) -> None:
+      super().__init__()
 
-        self.noise_dim = noise_dim
-        self.num_classes = num_classes
-        self.device = device
+      self.noise_dim = noise_dim
+      self.num_classes = num_classes
+      self.device = device
+      self.fc1 = nn.Sequential(
+          nn.Linear(noise_dim + num_classes, hidden_dim),
+          nn.BatchNorm1d(hidden_dim),
+          nn.ReLU()
+      )
+      self.fc = nn.Linear(hidden_dim, feature_dim)
 
-        self.fc1 = nn.Sequential(
-            nn.Linear(noise_dim + num_classes, hidden_dim),
-            nn.BatchNorm1d(hidden_dim),
-            nn.ReLU()
-        )
+  def forward(self, labels):
+      batch_size = labels.shape[0]
+      eps = torch.rand((batch_size, self.noise_dim), device=self.device) # sampling from Gaussian
+      y_input = F.one_hot(labels, self.num_classes)
+      z = torch.cat((eps, y_input), dim=1)
 
-        self.fc = nn.Linear(hidden_dim, feature_dim)
-
-    def forward(self, labels):
-        batch_size = labels.shape[0]
-        eps = torch.rand((batch_size, self.noise_dim), device=self.device) # sampling from Gaussian
-
-        y_input = F.one_hot(labels, self.num_classes)
-        z = torch.cat((eps, y_input), dim=1)
-
-        z = self.fc1(z)
-        z = self.fc(z)
-
-        return z
+      z = self.fc1(z)
+      z = self.fc(z)
+      return z
 
 class Server(BasicServer):
+  def initialize(self):
+    BasicServer.initialize(self)
+    self.init_algo_para({'server_epochs':500,'num_classes':10,'noise_dim':256,'generator_learning_rate':0.05,
+              'localize_feature_extractor':False,'hidden_dim':256,'learning_rate_decay_gamma':0.99,'alpha':0.2})
+    self.batch_size = option['batch_size']
+    self.generative_model = Generative_fedGEN(self.noise_dim, self.num_classes, self.hidden_dim, self.model.fc.in_features, self.device).to(self.device)
+    self.generative_optimizer = torch.optim.Adam(
+        params=self.generative_model.parameters(),
+        lr=self.generator_learning_rate, betas=(0.9, 0.999),
+        eps=1e-08, weight_decay=0, amsgrad=False)
+    self.generative_learning_rate_scheduler = torch.optim.lr_scheduler.ExponentialLR(
+        optimizer=self.generative_optimizer, gamma=self.learning_rate_decay_gamma)
+    self.loss = nn.CrossEntropyLoss()
+
+    self.qualified_labels = []
+    for client in self.clients:
+      client.sample_per_class,client.train_samples = get_loc_data(client.id,self.option['task'])
+      for yy in range(self.num_classes):
+        self.qualified_labels.extend([yy for _ in range(int(client.sample_per_class[yy].item()))])
+    for client in self.clients:
+      client.qualified_labels = self.qualified_labels
+
   def pack(self, client_id, mtype=0, *args, **kwargs):
     return {
         "model": copy.deepcopy(self.model),
@@ -53,33 +73,6 @@ class Server(BasicServer):
     self.train_generator(models,self.selected_clients)
     self.model = self.aggregate(models)
     return len(models) > 0
-  def initialize(self):
-    BasicServer.initialize(self)
-    args = self.option['args']
-    # self.load_model()
-    self.Budget = []
-    self.batch_size = option['batch_size']
-    self.generative_model = Generative_fedGEN(args.noise_dim, args.num_classes, args.hidden_dim, self.model.fc.in_features, self.device).to(self.device)
-    self.generative_optimizer = torch.optim.Adam(
-        params=self.generative_model.parameters(),
-        lr=args.generator_learning_rate, betas=(0.9, 0.999),
-        eps=1e-08, weight_decay=0, amsgrad=False)
-    self.generative_learning_rate_scheduler = torch.optim.lr_scheduler.ExponentialLR(
-        optimizer=self.generative_optimizer, gamma=args.learning_rate_decay_gamma)
-    self.loss = nn.CrossEntropyLoss()
-
-    self.qualified_labels = []
-    for client in self.clients:
-      client.sample_per_class,client.train_samples = get_loc_data(client.id,self.option['task'])
-      for yy in range(args.num_classes):
-        self.qualified_labels.extend([yy for _ in range(int(client.sample_per_class[yy].item()))])
-    for client in self.clients:
-      client.qualified_labels = self.qualified_labels
-
-    self.server_epochs = args.server_epochs
-    self.localize_feature_extractor = args.localize_feature_extractor
-    if self.localize_feature_extractor:
-      self.global_model = copy.deepcopy(args.model.fc)
 
   def train_generator(self,uploaded_models,selected_clients):
     grad_True(self.generative_model)
@@ -89,7 +82,6 @@ class Server(BasicServer):
         labels = np.random.choice(self.qualified_labels, self.batch_size)
         labels = torch.LongTensor(labels).to(self.device)
         z = self.generative_model(labels)
-
         logits = 0
         for w, model in zip(uploaded_weights, uploaded_models):
             model.eval()
@@ -115,40 +107,21 @@ class Server(BasicServer):
     for i, w in enumerate(uploaded_weights):
       uploaded_weights[i] = w / tot_samples
     return uploaded_weights
-class Client(BasicClient):
-  def unpack(self, received_pkg):
-    return received_pkg['model'],received_pkg['generative_model']
-  def reply(self, svr_pkg):
-    model,generative_model = self.unpack(svr_pkg) #svr_pkg (dict): the package received from the server
-    self.train(model,generative_model)
-    cpkg = self.pack(model)
-    return cpkg #client_pkg (dict): the package to be send to the server
+class Client(extraClient):
+  def extra_received(self, received_pkg):
+    self.generative_model = received_pkg['generative_model']
   def initialize(self, *args, **kwargs):
-    self.args = self.option['args']
-    self.localize_feature_extractor = self.args.localize_feature_extractor
     self.loss = nn.CrossEntropyLoss()
+  def prepare_train(self,model):
+    grad_False(self.generative_model)
+    self.generative_model.eval()
+  def local_training_with_extra_calculate(self, model, loss, outputs, batch_data):
+    labels = np.random.choice(self.qualified_labels, self.batch_size)
+    labels = torch.LongTensor(labels).to(self.device)
+    with torch.no_grad():
+      z = self.generative_model(labels)
+    return loss + self.alpha * self.loss(model.fc(z), labels)
 
-  def train(self, model, generative_model):
-    grad_True(model)
-    model.train()
-    grad_False(generative_model)
-    generative_model.eval()
-    optimizer = self.calculator.get_optimizer(model, lr=self.learning_rate, weight_decay=self.weight_decay, momentum=self.momentum)
-    for iter in range(self.num_steps):
-      batch_data = self.get_batch_data()
-      model.zero_grad()
-      loss = self.calculator.compute_loss(model, batch_data)['loss']
-      x, y = batch_data
-      x.to(device=self.device)
-      labels = np.random.choice(self.qualified_labels, self.batch_size)
-      labels = torch.LongTensor(labels).to(self.device)
-      with torch.no_grad():
-        z = generative_model(labels)
-      loss += self.args.alpha * self.loss(model.fc(z), labels)
-      loss.backward()
-      if self.clip_grad>0:torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=self.clip_grad)
-      optimizer.step()
-    return
 class FedGen:
   Server=Server
   Client=Client
