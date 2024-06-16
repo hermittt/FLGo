760f15680554495617edec4d9c7a7edbc790cdde
hermittt <499655727@qq.com>
2024-06-16 09:22:45 +0000

    task

---
+++ flgo/my_algorithm/fedGKD.py
@@ -0,0 +1,67 @@
+from flgo.utils import fmodule
+import flgo
+import numpy as np
+import torch,copy
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from flgo.algorithm.fedbase import BasicServer, BasicClient
+from flgo.my_algorithm.extra import extraServer, extraClient
+from flgo.my_algorithm.my_utils import grad_False,grad_True,KL_Loss_equivalent
+
+class GKDServer(extraServer):
+  def initialize(self):
+    self.init_algo_para({'buffer_len':5,'T':'1','esb_w':5.0,'distill_w':'0.001*self.round'})#buffer_len:<0无缓存，=0按类别，>0最近k个
+    if self.buffer_len > 0:
+      self.buffer = []
+    elif self.buffer_len == 0:
+      self.buffer = [[i, None] for i in range(len(self.clients))] #初始化空的客户端模型列表
+  def extra_received(self,models,recv):
+    if self.buffer_len == 0:
+      for client_id,client_model in zip(self.selected_clients,models):
+        grad_False(client_model)
+        client_model.eval()
+        self.buffer[client_id][1]=client_model
+  def extra_after(self):
+    if self.buffer_len > 0:
+      if len(self.buffer) >= self.buffer_len:
+        self.buffer.pop(0)
+      self.buffer.append([0,copy.deepcopy(self.model)]) #0没有用，纯接口占位
+  def pack(self, client_id, mtype=0, *args, **kwargs):
+    ensemble_models = []
+    for client_idx, local_model in self.buffer:
+      if local_model == None:
+        continue
+      else:
+        ensemble_models.append(local_model)
+    ensemble_model = self.aggregate(ensemble_models)
+    return {
+        "model": copy.deepcopy(self.model),
+        "ensemble_model": copy.deepcopy(ensemble_model),
+        "round": self.current_round,
+    }
+class GKDClient(extraClient):
+  def initialize(self, *args, **kwargs):
+    self.ensemble_model = None
+    self.loss = nn.CrossEntropyLoss()
+    self.KL_loss = KL_Loss_equivalent()
+  def extra_received(self, received_pkg):
+    self.ensemble_model = received_pkg['ensemble_model']
+    self.round = received_pkg['round']
+  def prepare_train(self,model):
+    self.teacher_model = copy.deepcopy(model)
+    grad_False(self.teacher_model)
+    self.teacher_model.eval()
+    grad_False(self.ensemble_model)
+    self.ensemble_model.eval()
+  def local_training_with_extra_calculate(self, model, loss, outputs, batch_data):
+    x, y = batch_data
+    x = x.to(self.device)
+    C_teacher = self.teacher_model(x).detach()
+    C_ensemble = self.ensemble_model(x).detach()
+    distill_loss =self.KL_loss(C_teacher,outputs,T=eval(self.T))+self.esb_w*self.KL_loss(C_ensemble,outputs,T=eval(self.T))
+    return loss + distill_loss * eval(self.distill_w)
+class FedGKD:
+  Server=GKDServer
+  Client=GKDClient
