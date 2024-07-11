a0944c17044dd0811e5054c437e05cd0237dedc1
hermittt <499655727@qq.com>
2024-07-11 16:52:03 +0000

    task

--- flgo/my_algorithm/extra.py
+++ flgo/my_algorithm/extra.py
@@ -44,6 +44,7 @@ class extraClient(BasicClient):
       loss.backward()
       if self.clip_grad>0:torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=self.clip_grad)
       optimizer.step()
+      self.after_iter(model,batch_data)
       if (iter+1) % 50 == 0 and self.trainning_output!={}:
         # 按固定格式将每一个元素连接成一个字符串，并输出
         output = ", ".join([f"{key}：{value:.2f}" if isinstance(value, float) 
@@ -51,6 +52,8 @@ class extraClient(BasicClient):
         print(output)
     self.after_train(model)
     return
+  def after_iter(self,model,batch_data):
+    pass #训练生成模型
   def prepare_train(self,model):
     pass #设置一些辅助模型的参数冻结等
   def after_train(self,model):
