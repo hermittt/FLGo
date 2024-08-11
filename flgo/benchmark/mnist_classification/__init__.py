b856f10bf675c5b83d3750033ef49c5be02dc0af
Hhhhhh <499655727@qq.com>
2024-08-11 20:57:37 +0800

    Update __init__.py

--- flgo/benchmark/mnist_classification/__init__.py
+++ flgo/benchmark/mnist_classification/__init__.py
@@ -23,10 +23,10 @@ the term `IIDPartitioner` is replaced by `DirichletPartitioner`. In addition, pa
 be input by `para`.
 """
 import flgo.benchmark.toolkits.visualization
-from flgo.benchmark.mnist_classification.model import cnn
+from flgo.benchmark.mnist_classification.model import cnn_no_bn
 import flgo.benchmark.toolkits.partition
 
 default_partitioner = flgo.benchmark.toolkits.partition.IIDPartitioner
 default_partition_para = {'num_clients':100}
-default_model = cnn
+default_model = cnn_no_bn
 visualize = flgo.benchmark.toolkits.visualization.visualize_by_class
