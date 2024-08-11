38220fb98493e9caa3c26cfd1591fa2df6201f77
Hhhhhh <499655727@qq.com>
2024-08-11 20:59:11 +0800

    Update __init__.py

--- flgo/benchmark/fashion_classification/__init__.py
+++ flgo/benchmark/fashion_classification/__init__.py
@@ -1,8 +1,8 @@
 import flgo.benchmark.toolkits.visualization
-from flgo.benchmark.fashion_classification.model import cnn
+from flgo.benchmark.fashion_classification.model import cnn_no_bn
 import flgo.benchmark.toolkits.partition
 
 default_partitioner = flgo.benchmark.toolkits.partition.IIDPartitioner
 default_partition_para = {'num_clients':100}
-default_model = cnn #换成和mnist一样
+default_model = cnn_no_bn #换成和mnist一样
 visualize = flgo.benchmark.toolkits.visualization.visualize_by_class
