697ef05629fdd767a580e6bfafd56ec9ec9d2d3a
Hhhhhh <499655727@qq.com>
2024-08-29 19:05:42 +0800

    Update fedKF.py

--- flgo/my_algorithm/fedKF.py
+++ flgo/my_algorithm/fedKF.py
@@ -132,7 +132,7 @@ class KFServer(GKDServer): #FedGKD，FedKF通用，传输额外的缓存模型
         'T': '10',
         'esb_w': 1.0,
         'distill_w1': '0',#'0.025*self.round',
-        'distill_w2': '0.1*self.round',
+        'distill_w2': '0.01*self.round',
         'min_round': 5,
         'generator_learning_rate': 5e-4,
         'act_w': 1e-4,
