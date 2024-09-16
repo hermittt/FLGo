d95c760afc185f071130656027b546447cf2d706
Hhhhhh <499655727@qq.com>
2024-09-16 19:42:05 +0800

    Update fedKF.py

--- flgo/my_algorithm/fedKF.py
+++ flgo/my_algorithm/fedKF.py
@@ -246,7 +246,8 @@ class KFClient(GKDClient):
       self.G.eval()
       G = self.G(self.sample_z_,self.sample_y_)[0]
       self.show([G])
-
+      self.save_G()
+  def save_G(self):
       # 保存 self.G 模型到一个文件，例如 'model_G.pth'
       torch.save(self.G.state_dict(), "model_G.pth")
       # 创建 Artifact 并将模型文件加入
@@ -256,7 +257,7 @@ class KFClient(GKDClient):
       wandb.log_artifact(artifact)
             
   def show(self,imgs,x=None):
-    name=str(self.id)
+    name=self.rslt_path+'.png'
     imgs_out=[]
     for i, img in enumerate(imgs):
       path=self.rslt_path + '/%2d[%d]' % (self.round,i) + '.png'
