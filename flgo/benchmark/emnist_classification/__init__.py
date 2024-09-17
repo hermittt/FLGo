a6d102cb63d393c4e91a0638209f8c4a6e7983c3
Hhhhhh <499655727@qq.com>
2024-09-18 04:13:58 +0800

    Update __init__.py

--- flgo/benchmark/emnist_classification/__init__.py
+++ flgo/benchmark/emnist_classification/__init__.py
@@ -4,5 +4,5 @@ import flgo.benchmark.toolkits.partition
 
 default_partitioner = flgo.benchmark.toolkits.partition.IIDPartitioner
 default_partition_para = {'num_clients':100}
-default_model = cnn
+default_model = cnn_no_bn
 visualize = flgo.benchmark.toolkits.visualization.visualize_by_class
