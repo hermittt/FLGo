e0b25a7309a5125fa146b1162250ae5b71baf991
Hhhhhh <499655727@qq.com>
2024-09-18 04:54:47 +0800

    Update core.py

--- flgo/benchmark/emnist_classification/core.py
+++ flgo/benchmark/emnist_classification/core.py
@@ -1,20 +1,3 @@
-import os
-from flgo.benchmark.toolkits.cv.classification import GeneralCalculator, FromDatasetPipe, FromDatasetGenerator
-from .config import train_data, test_data
-
-class TaskGenerator(FromDatasetGenerator):
-    def __init__(self):
-        super(TaskGenerator, self).__init__(benchmark=os.path.split(os.path.dirname(__file__))[-1],
-                                            train_data=train_data, test_data=test_data)
-
-class TaskPipe(FromDatasetPipe):
-    def __init__(self, task_path):
-        super(TaskPipe, self).__init__(task_path, train_data, test_data)
-
-TaskCalculator = GeneralCalculator
-
-
-'''
 import torchvision
 from flgo.benchmark.toolkits.cv.classification import BuiltinClassGenerator, BuiltinClassPipe, GeneralCalculator
 import flgo.benchmark
@@ -39,4 +22,3 @@ class TaskPipe(BuiltinClassPipe):
         super(TaskPipe, self).__init__(task_path, builtin_class, transform)
 
 TaskCalculator = GeneralCalculator
-'''
