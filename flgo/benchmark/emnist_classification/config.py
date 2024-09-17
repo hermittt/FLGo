58c36902aaea3c433ad007afdf0430e3b1e2d1f9
Hhhhhh <499655727@qq.com>
2024-09-18 04:50:08 +0800

    Update config.py

--- flgo/benchmark/emnist_classification/config.py
+++ flgo/benchmark/emnist_classification/config.py
@@ -6,5 +6,5 @@ transform = torchvision.transforms.Compose(
      torchvision.transforms.Normalize((0.1307,), (0.3081,))]
 )
 path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'RAW_DATA', 'MNIST')
-train_data = torchvision.datasets.MNIST(root=path, train=True, download=True, transform=transform)
-test_data = torchvision.datasets.MNIST(root=path, train=False, download=True, transform=transform)
+train_data = torchvision.datasets.EMNIST(root=path, train=True, download=True, transform=transform)
+test_data = torchvision.datasets.EMNIST(root=path, train=False, download=True, transform=transform)
