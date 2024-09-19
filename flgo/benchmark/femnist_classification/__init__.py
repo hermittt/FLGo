c3fc65bda44ee2d88456bb58845e901c37cd94ae
Hhhhhh <499655727@qq.com>
2024-09-19 15:34:09 +0800

    Update __init__.py

--- flgo/benchmark/femnist_classification/__init__.py
+++ flgo/benchmark/femnist_classification/__init__.py
@@ -1,8 +1,8 @@
 import flgo.benchmark.toolkits.visualization
-from flgo.benchmark.femnist_classification.model import cnn
+from flgo.benchmark.femnist_classification.model import cnn_no_bn
 import flgo.benchmark.toolkits.partition
 
 default_partitioner = flgo.benchmark.toolkits.partition.IIDPartitioner
 default_partition_para = {'num_clients':100}
-default_model = cnn
+default_model = cnn_no_bn
 visualize = flgo.benchmark.toolkits.visualization.visualize_by_class
